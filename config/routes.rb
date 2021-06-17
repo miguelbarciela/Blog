7771ae18b71141ae55827f8a0dcecc860a7238ad
Miguel Barciela <miguelbarciela@hotmail.com>
2021-06-17 21:04:06 +0200

    created Categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,14 @@
 Rails.application.routes.draw do
+  resources :categories
   devise_for :users
    get 'home/index'
    root to: "home#index"
+   
+   resources :articles do
+    get "user/:user_id", to: "articles#from_author", on: :collection   
+    #resources :users
+   end
 
-   get "articles/user/:user_id", to: "articles#from_author"
-
-   get "articles", to: "articles#index"
-   get "articles/new", to: "articles#new", as: :new_articles
-   get "articles/:id", to: "articles#show"
-   get "articles/:id/edit", to: "articles#edit", as: :edit
-
-   patch "articles/:id", to: "articles#update", as: :article
-   post "articles", to: "articles#create"
-   delete "articles/:id", to: "articles#destroy"
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
