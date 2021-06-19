3868b8335a5c883392be867ee4e6c5172855f439
Miguel Barciela <miguelbarciela@hotmail.com>
2021-06-19 13:00:08 +0200

    added Comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
     has_rich_text :content
     belongs_to :user
-    has_many :comments
+    has_many :comments, dependent: :destroy
 
 
     has_many :has_categories
