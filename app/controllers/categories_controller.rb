d573a806a07c8e7856f909e93d602e7e9eab8308
Miguel Barciela <miguelbarciela@hotmail.com>
2021-06-19 18:36:07 +0200

    added photo perfil

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: %i[ show edit update destroy ]
+  http_basic_authenticate_with name: "miguel", password: "123456", except: [:index]
 
   # GET /categories or /categories.json
   def index
